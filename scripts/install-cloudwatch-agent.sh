f3d2414064900dc4397b5a01c3c75fd812ccc428
Gaurang Davda <davda.g@husky.neu.edu>
2019-03-20 02:22:19 -0400

    cloud watch

--- scripts/install-cloudwatch-agent.sh
+++ scripts/install-cloudwatch-agent.sh
@@ -6,3 +6,10 @@ wget https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-
 echo "cloud watch for us-east-1 downloaded successfully"
 chmod +x ./amazon-cloudwatch-agent.rpm
 sudo rpm -U ./amazon-cloudwatch-agent.rpm
+echo "gaurang"
+chkconfig --list
+echo "gaurang chkconfig hogya"
+service --status-all
+echo "gaurang service hogya"
+systemctl list-unit-files --type=service
+echo "gaurang hogya sab khatam"
